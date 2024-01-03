5bd9e0d16ae9e3aebb820fcf90bb56b88597db36
Mateusz Kadula <mateusz.kadula@icloud.com>
2024-01-03 22:34:39 +0100

    Implemented HTMLTemplate class

--- htmplate/template.py
+++ htmplate/template.py
@@ -8,6 +8,16 @@ import re
 
 class FieldAbstract(ABC):
 
+    _registry: set[FieldAbstract] = set()
+
+    def __init_subclass__(cls, **kwargs):
+        super().__init_subclass__(**kwargs)
+        cls._registry.add(cls)
+
+    @staticmethod
+    def get_registry() -> frozenset[FieldAbstract]:
+        return frozenset(FieldAbstract._registry)
+
     class FieldCreationError(Exception):
         pass
 
@@ -23,6 +33,9 @@ class FieldAbstract(ABC):
     @abstractmethod
     def get_regex(cls) -> str:
         ...
+
+    def get_field_content(self) -> str:
+        return self.field_content
 
     @classmethod
     def is_correct_field_content(cls, field_content: str) -> bool:
@@ -61,7 +74,7 @@ class DataField(FieldAbstract):
 
 class FileField(FieldAbstract):
 
-    regex = r'{%\s*!file\s+([a-zA-Z0-9_.,~\\/-]+)\s*%}'
+    regex = r'{%\s*!file\s+([a-zA-Z0-9_.,~\\/ -]+)\s*%}'
 
     def __init__(self, field_content: str):
         super().__init__(field_content)
@@ -83,11 +96,52 @@ class FileField(FieldAbstract):
 
 
 class HTMLTemplate:
+
     def __init__(self, template: str):
         self.template = template
+        self._fields_cache: None | list[tuple[int, int, FieldAbstract]] = None
 
-    def fields(self) -> frozenset[str]:
-        ...
+    def fields(self) -> list[FieldAbstract]:
+        if self._fields_cache is None:
+            self._fields_cache = []
+            for field_cls in FieldAbstract.get_registry():
+                for match in re.finditer(field_cls.get_regex(), self.template):
+                    self._fields_cache.append((match.start(), match.end(), field_cls.init(match.group(0))))
+            self._fields_cache.sort(key=lambda x: x[0])
 
-    def make(self, values: dict) -> Self:
-        ...
+        return self._fields_cache.copy()
+
+    @classmethod
+    def _make_inner(cls, instance: Self, values: dict[str, Any]) -> tuple[Self, bool]:
+        out_template = []
+        last_end = 0
+        filled_anything = False
+
+        for start, end, field in instance.fields():
+            if start < last_end:
+                raise ValueError('Overlapping fields')  # TODO: change to custom exception
+
+            out_template.append(instance.template[last_end:start])
+            value, is_correct = field.execute(values)
+            if is_correct:
+                out_template.append(value)
+                filled_anything = True
+            else:
+                out_template.append(field.get_field_content())
+            last_end = end
+
+        out_template.append(instance.template[last_end:])
+        return instance.__class__(''.join(out_template)), filled_anything
+
+    def make(self, values: dict[str, Any], iter_limit: int = -1) -> Self:
+        instance = self
+        filled_anything = True
+        iterations = 0
+
+        while filled_anything and (iterations < iter_limit or iter_limit == -1):
+            instance, filled_anything = self._make_inner(instance, values)
+            iterations += 1
+        if filled_anything:
+            raise ValueError('Iteration limit reached')
+
+        return instance
