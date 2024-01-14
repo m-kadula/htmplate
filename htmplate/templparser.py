40893b5f32b4c7d65cea7a9c7df643067f806f3c
Mateusz Kadula <mateusz.kadula@icloud.com>
2024-01-15 00:04:43 +0100

    template parser and tests

---
+++ htmplate/templparser.py
@@ -0,0 +1,235 @@
+from pathlib import Path
+from typing import Any
+import re
+
+from .parsing import Parser, SingleField, ControlField, SimpleLexer, ActiveToken, InactiveToken
+
+
+def parse_context_path(context: dict, path: str) -> Any | None:
+    path = path.split('.')
+    for p in path:
+        if isinstance(context, list):
+            if not p.isnumeric():
+                return None
+            else:
+                context = context[int(p)]
+        else:
+            if p not in context:
+                return None
+            else:
+                context = context[p]
+    return context
+
+
+class DataField(SingleField):
+
+    field = SingleField.make_field(r'\s*([0-9a-zA-Z_\.]+)\s*')
+
+    def render(self, context: Any, inner_extra: dict) -> str:
+        assert isinstance(self.content, ActiveToken)
+        reg = re.compile(self.field.signature)
+        match = reg.match(self.content.instruction)
+        name = match.group(1)
+        inner = parse_context_path(context, name)
+        if inner is not None:
+            return self.parser.parse(inner, context)
+        else:
+            return self.content.field_content
+
+
+class FileInclude(SingleField):
+
+    field = SingleField.make_field(r'\s*include\s+([0-9a-zA-Z_/\.~-]+)\s+(with\s+([0-9a-zA-Z_\.]+))?\s*')
+
+    def render(self, context: Any, inner_extra: dict) -> str:
+        assert isinstance(self.content, ActiveToken)
+        assert 'fs_root' in self.extra_context
+
+        fs_root = Path(self.extra_context['fs_root'])
+        reg = re.compile(self.field.signature)
+        match = reg.match(self.content.instruction)
+        path = match.group(1)
+        file = (fs_root / path).resolve()
+
+        if match.group(2) is not None:
+            name = match.group(3)
+            inner = parse_context_path(context, name)
+            if inner is not None:
+                context = inner
+            else:
+                return self.content.field_content
+
+        if not file.exists():
+            return self.content.field_content
+        with open(file, encoding='utf-8') as f:
+            content = f.read()
+        return self.parser.parse(content, context, **self.extra_context)
+
+
+class IterField(ControlField):
+
+    initial_fields = ControlField.initial(
+        ('for', r'\s*for\s+([0-9a-zA-Z_]+)\s+in\s+([0-9a-zA-Z_\.]+)\s*')
+    )
+
+    middle_fields = ControlField.middle()
+
+    final_fields = ControlField.final(
+        ('end for', r'\s*end\s+for\s*')
+    )
+
+    body = ControlField.make_body(
+        ('for',),
+        ('end for',)
+    )
+
+    def render(self, context: Any, inner_extra: dict) -> str:
+        out: list[str] = []
+        signature, token, node = self.content[0]
+        reg = re.compile(signature.signature)
+        match = reg.match(token.instruction)
+        value_name = match.group(1)
+        iterable_name = match.group(2)
+
+        iterable = parse_context_path(context, iterable_name)
+        if iterable is None:
+            return self.get_original()
+
+        if not isinstance(iterable, list):
+            return self.get_original()
+
+        for value in iterable:
+            mut_context = {value_name: value}
+            content = node.render(mut_context | context, inner_extra)
+            out.append(content)
+
+        return ''.join(out)
+
+
+class DictIterField(ControlField):
+
+    initial_fields = ControlField.initial(
+        ('for', r'\s*for\s+([0-9a-zA-Z_]+),\s*([0-9a-zA-Z_]+)\s+in\s+([0-9a-zA-Z_\.]+)\s*')
+    )
+
+    middle_fields = ControlField.middle()
+
+    final_fields = ControlField.final(
+        ('end for', r'\s*end\s+for\s*')
+    )
+
+    body = ControlField.make_body(
+        ('for',),
+        ('end for',)
+    )
+
+    def render(self, context: Any, inner_extra: dict) -> str:
+        out: list[str] = []
+        signature, token, node = self.content[0]
+        reg = re.compile(signature.signature)
+        match = reg.match(token.instruction)
+        key_name = match.group(1)
+        value_name = match.group(2)
+        iterable_name = match.group(3)
+
+        iterable = parse_context_path(context, iterable_name)
+        if iterable is None:
+            return self.get_original()
+
+        if not isinstance(iterable, dict):
+            return self.get_original()
+
+        for key, value in iterable.items():
+            mut_context = {key_name: key, value_name: value}
+            content = node.render(mut_context | context, inner_extra)
+            out.append(content)
+
+        return ''.join(out)
+
+
+class ConditionalField(ControlField):
+
+    initial_fields = ControlField.initial(
+        ('if', r'\s*if\s+([0-9a-zA-Z_\.]+)\s*'),
+        ('if', r'\s*if:exists\s+([0-9a-zA-Z_\.]+)\s*')
+    )
+
+    middle_fields = ControlField.middle(
+        ('elif', r'\s*elif\s+([0-9a-zA-Z_\.]+)\s*'),
+        ('elif', r'\s*elif:exists\s+([0-9a-zA-Z_\.]+)\s*'),
+        ('else', r'\s*if:else\s*'),
+    )
+
+    final_fields = ControlField.final(
+        ('end if', r'\s*end\s+if\s*'),
+    )
+
+    body = ControlField.make_body(
+        ('if',),
+        ('elif', (0, float('inf'))),
+        ('else', (0, 1)),
+        ('end if',)
+    )
+
+    def render(self, context: Any, inner_extra: dict) -> str:
+        signature = None
+        node = None
+
+        for signature, token, node in self.content:
+            if signature.name == 'else':
+                break
+
+            check_sig = self.initial_fields[1] if signature.name == 'if' else self.middle_fields[1]
+
+            reg = re.compile(signature.signature)
+            match = reg.match(token.instruction)
+            name = match.group(1)
+            value = parse_context_path(context, name)
+
+            if signature == check_sig:
+                if value is not None:
+                    tmp = node.render(context, inner_extra)
+                    return self.parser.parse(tmp, context, **self.extra_context)
+            else:
+                if value is None:
+                    return self.get_original()
+                if bool(value):
+                    tmp = node.render(context, inner_extra)
+                    return self.parser.parse(tmp, context, **self.extra_context)
+
+        if signature is not None and signature.name == 'else':
+            assert node is not None
+            tmp = node.render(context, inner_extra)
+            return self.parser.parse(tmp, context, **self.extra_context)
+
+        return ''
+
+
+class ContextField(ControlField):
+
+    initial_fields = ControlField.initial(
+        ('context', r'\s*context\s+([0-9a-zA-Z_\.]+)\s*'),
+    )
+
+    middle_fields = ControlField.middle()
+
+    final_fields = ControlField.final(
+        ('end context', r'\s*end\s+context\s*'),
+    )
+
+    body = ControlField.make_body(
+        ('context',),
+        ('end context',)
+    )
+
+    def render(self, context: Any, inner_extra: dict) -> str:
+        signature, token, node = self.content[0]
+        reg = re.compile(signature.signature)
+        match = reg.match(token.instruction)
+        value_name = match.group(1)
+        value = parse_context_path(context, value_name)
+        if value is not None:
+            tmp = node.render(value, inner_extra)
+            return self.parser.parse(tmp, context, **self.extra_context)
+        else:
+            return self.get_original()
---
+++ tests/test_templparser.py
@@ -0,0 +1,202 @@
+import unittest
+from pathlib import Path
+
+from bs4 import BeautifulSoup
+
+from htmplate.parsing import Parser, SimpleLexer
+from htmplate.templparser import DataField, IterField, ConditionalField, ContextField, FileInclude
+
+
+RESOURCE_DIR = Path(__file__).parent / 'resources'
+
+
+class ParsingTest(unittest.TestCase):
+
+    def setUp(self):
+        self.parser = Parser(
+            SimpleLexer(),
+            [DataField, IterField, ConditionalField, ContextField, FileInclude])
+
+    def test_basic(self):
+        text = "Hi, my name is {{ name }}. and I am from {{ country }}. I am {{ age }} years old."
+        context = {'name': 'John', 'country': 'USA', 'age': '20'}
+        parsed = self.parser.parse(text, context)
+        self.assertEqual(parsed, 'Hi, my name is John. and I am from USA. I am 20 years old.')
+
+        text = "Hi, my name is {{ name }}. and I am from {{ nested }}. I am {{ age }} years old."
+        context = {'name': 'John', 'nested': "{{ country }}, {{ town }}", 'age': '20', 'country': 'USA', 'town': 'NY'}
+        parsed = self.parser.parse(text, context)
+        self.assertEqual(parsed, 'Hi, my name is John. and I am from USA, NY. I am 20 years old.')
+
+    def test_no_val_in_context(self):
+        text = "Hi, my name is {{ name }}. and I am from {{ country }}. I am {{ age }} years old."
+        context = {'name': 'John', 'age': '20'}
+        parsed = self.parser.parse(text, context)
+        self.assertEqual(parsed, 'Hi, my name is John. and I am from {{ country }}. I am 20 years old.')
+
+    def test_no_val_in_context_if(self):
+        text = "{{ if condition }}my name is: {{ name }}{{ if:else }}i wont tell you my name.{{ end if }}"
+        context = {}
+        parsed = self.parser.parse(text, context)
+        self.assertEqual(parsed, text)
+
+    def test_iter(self):
+        text = "{{ for item in items }}item: {{ item }}\n{{ end for }} hi"
+        context = {'items': ['a', 'b', 'c']}
+        parsed = self.parser.parse(text, context)
+        self.assertEqual(parsed, 'item: a\nitem: b\nitem: c\n hi')
+
+        text = "{{ for list in lists }}{{ for item in list }}item: {{ item }}\n{{ end for }}\n{{ end for }}"
+        context = {'lists': [['a', 'b'], ['c', 'd']]}
+        parsed = self.parser.parse(text, context)
+        self.assertEqual(parsed, 'item: a\nitem: b\n\nitem: c\nitem: d\n\n')
+
+        text = "{{ for item in items }}item: {{ item }}\n{{ end for }}"
+        context = {'items': []}
+        parsed = self.parser.parse(text, context)
+        self.assertEqual(parsed, '')
+
+    def test_if(self):
+        text = "{{ if condition }}{{ name }}{{ end if }}"
+        context = {'condition': True, 'name': 'John'}
+        parsed = self.parser.parse(text, context)
+        self.assertEqual(parsed, 'John')
+
+        text = "{{ if condition }}my name is: {{ name }}{{ if:else }}i wont tell you my name.{{ end if }}"
+        parsed = self.parser.parse(text, {'condition': False, 'name': 'John'})
+        self.assertEqual(parsed, 'i wont tell you my name.')
+        parsed = self.parser.parse(text, {'condition': True, 'name': 'John'})
+        self.assertEqual(parsed, 'my name is: John')
+
+        text = ("{{ if condition1 }}{{ name1 }}{{ elif condition2 }}"
+                "{{ name2 }}{{ elif condition3 }}{{ name3 }}{{ end if }}")
+        context = {'condition1': False, 'condition2': True, 'condition3': False,
+                   'name1': 'John', 'name2': 'Mike', 'name3': 'Jack'}
+        parsed = self.parser.parse(text, context)
+        self.assertEqual(parsed, 'Mike')
+        context = {'condition1': False, 'condition2': False, 'condition3': True,
+                   'name1': 'John', 'name2': 'Mike', 'name3': 'Jack'}
+        parsed = self.parser.parse(text, context)
+        self.assertEqual(parsed, 'Jack')
+        context = {'condition1': True, 'condition2': False, 'condition3': False,
+                   'name1': 'John', 'name2': 'Mike', 'name3': 'Jack'}
+        parsed = self.parser.parse(text, context)
+        self.assertEqual(parsed, 'John')
+
+        text = "{{ if condition1 }}{{ name1 }}{{ elif condition2 }}{{ name2 }}{{ if:else }}nobody{{ end if }}"
+        context = {'condition1': False, 'condition2': True, 'name1': 'John', 'name2': 'Mike'}
+        parsed = self.parser.parse(text, context)
+        self.assertEqual(parsed, 'Mike')
+        context = {'condition1': False, 'condition2': False, 'name1': 'John', 'name2': 'Mike'}
+        parsed = self.parser.parse(text, context)
+        self.assertEqual(parsed, 'nobody')
+        context = {'condition1': True, 'condition2': False, 'name1': 'John', 'name2': 'Mike'}
+        parsed = self.parser.parse(text, context)
+        self.assertEqual(parsed, 'John')
+
+    def test_if_exists(self):
+        text = "{{ if:exists name }}{{ name }}{{ if:else }}{{ name2 }}{{ end if }}"
+        context = {'name': 'John', 'name2': 'Mike'}
+        parsed = self.parser.parse(text, context)
+        self.assertEqual(parsed, 'John')
+        context = {'name2': 'Mike'}
+        parsed = self.parser.parse(text, context)
+        self.assertEqual(parsed, 'Mike')
+
+    def test_context(self):
+        text = "{{ context context_name }}{{ name }}{{ end context }}"
+        context = {'context_name': {'name': 'John'}}
+        parsed = self.parser.parse(text, context)
+        self.assertEqual(parsed, 'John')
+
+        text = "{{ context null }}{{ name }}{{ end context }}"
+        context = {'context_name': {'name': 'John'}}
+        parsed = self.parser.parse(text, context)
+        self.assertEqual(parsed, text)
+
+    def test_recursive(self):
+        text = "outer {{ inner }} outer_end"
+        inner = "inner {{ inner2 }} inner_end"
+        inner2 = "inner2 {{ inner3 }} inner2_end"
+        context = {'inner': inner, 'inner2': inner2, 'inner3': 'ground'}
+        parsed = self.parser.parse(text, context)
+        self.assertEqual(parsed, 'outer inner inner2 ground inner2_end inner_end outer_end')
+
+    def test_file(self):
+        text = "<meta>{{ include test_footer.html }}</meta>"
+        with open(RESOURCE_DIR / 'test_footer_filled.html') as f:
+            expected = f.read()
+
+        context = {
+            "name": "John Doe",
+            "contact_info": {
+                "emails": ["john.doe@mail.com", "john.doe@gmail.com"],
+                "numbers": ["123456789", "123 456 789"]
+            }
+        }
+        parsed = self.parser.parse(text, context, fs_root=RESOURCE_DIR)
+        soup_got = BeautifulSoup(parsed, 'html.parser')
+        soup_expected = BeautifulSoup('<meta>' + expected + '</meta>', 'html.parser')
+        self.assertEqual(soup_got.prettify(), soup_expected.prettify())
+
+        text = "<meta>{{ include does_not_exist.html }}</meta>"
+        parsed = self.parser.parse(text, context, fs_root=RESOURCE_DIR)
+        self.assertEqual(parsed, text)
+
+        text = "<meta>{{ include test_footer.html with inner.innermost }}</meta>"
+        context = {
+            'inner': {
+                'innermost': {
+                    "name": "John Doe",
+                    "contact_info": {
+                        "emails": ["john.doe@mail.com", "john.doe@gmail.com"],
+                        "numbers": ["123456789", "123 456 789"]
+                    }
+                }
+            }
+        }
+
+        parsed = self.parser.parse(text, context, fs_root=RESOURCE_DIR)
+        soup_got = BeautifulSoup(parsed, 'html.parser')
+        self.assertEqual(soup_got.prettify(), soup_expected.prettify())
+
+    def test_whole(self):
+        text = """
+Hi my name is {{ name }}. I am {{ age }} years old.
+a random list:{{ context iteration }}{{ for list in lists }}{{ for item in list }}
+{{ if item }}    - {{ item }}{{ if:else }}    - empty :({{ end if }}{{ end for }}
+{{ end for }}
+{{ end context }}
+        """
+        context = {'name': 'John', 'age': '20', 'iteration': {'lists': [['one', 'two', 'three'], ['raz', 'dwa', '']]}}
+        parsed = self.parser.parse(text, context)
+        print(parsed)
+        expected = """
+Hi my name is John. I am 20 years old.
+a random list:
+    - one
+    - two
+    - three
+
+    - raz
+    - dwa
+    - empty :(
+"""
+        self.assertEqual(expected.strip(), parsed.strip())
+
+    def test_html(self):
+        with open(RESOURCE_DIR / 'test_footer.html') as f:
+            text = f.read()
+        with open(RESOURCE_DIR / 'test_footer_filled.html') as f:
+            expected = f.read()
+        context = {
+            "name": "John Doe",
+            "contact_info": {
+                "emails": ["john.doe@mail.com", "john.doe@gmail.com"],
+                "numbers": ["123456789", "123 456 789"]
+            }
+        }
+        parsed = self.parser.parse(text, context)
+        soup_got = BeautifulSoup(parsed, 'html.parser')
+        soup_expected = BeautifulSoup(expected, 'html.parser')
+        self.assertEqual(soup_got.prettify(), soup_expected.prettify())
