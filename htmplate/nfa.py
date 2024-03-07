32766c1ada09e38aae42c67fe96bcb270155dcae
Mateusz Kadula <mateusz.kadula@icloud.com>
2024-03-07 01:01:30 +0100

    adds regular expression type verification to field body + tests

--- htmplate/nfa.py
+++ htmplate/nfa.py
@@ -1,5 +1,6 @@
 from typing import Literal
 from dataclasses import dataclass
+from functools import reduce
 
 
 @dataclass
@@ -8,7 +9,7 @@ class ReNode:
 
 
 @dataclass
-class GroundNode:
+class GroundNode(ReNode):
     content: str
 
 
@@ -41,6 +42,11 @@ class ENFA:
             self.last_state += 1
             return self.last_state - 1
 
+        def add_transition(self, state: int, letter: str, s: set[int]):
+            if (state, letter) not in self.dict:
+                self.dict[(state, letter)] = set()
+            self.dict[(state, letter)].update(s)
+
     def __init__(self, start_state: int, end_state: int, states: range, dict_: dict[tuple[int, str], set[int]]):
         assert states.start == 0
         self.states = states
@@ -61,8 +67,8 @@ class ENFA:
         my_end_state = context.get_new_state()
         for item in node.content:
             start, end = cls._parse_any(item, context)
-            context.dict[(my_start_state, '')] = {start}
-            context.dict[(end, '')] = {my_end_state}
+            context.add_transition(my_start_state, '', {start})
+            context.add_transition(end, '', {my_end_state})
         return my_start_state, my_end_state
 
     @classmethod
@@ -72,36 +78,38 @@ class ENFA:
         prev_end = my_start_state
         for item in node.content:
             start, end = cls._parse_any(item, context)
-            context.dict[(prev_end, '')] = {start}
+            context.add_transition(prev_end, '', {start})
             prev_end = end
-        context.dict[(prev_end, '')] = {my_end_state}
+        context.add_transition(prev_end, '', {my_end_state})
         return my_start_state, my_end_state
 
     @classmethod
     def _parse_item(cls, node: ReItem, context: Context) -> tuple[int, int]:
+        assert node.max_ >= node.min_ and node.max_ > 0
         my_start_state = context.get_new_state()
         my_end_state = context.get_new_state()
         prev_end = my_start_state
         for i in range(node.min_):
             start, end = cls._parse_any(node.content, context)
-            context.dict[(prev_end, '')] = {start}
+            context.add_transition(prev_end, '', {start})
             prev_end = end
         if node.max_ != float('inf'):
             for i in range(node.min_, node.max_):
                 start, end = cls._parse_any(node.content, context)
-                context.dict[(prev_end, '')] = {start, my_end_state}
+                context.add_transition(prev_end, '', {start, my_end_state})
                 prev_end = end
+            context.add_transition(prev_end, '', {my_end_state})
         else:
             start, end = cls._parse_any(node.content, context)
-            context.dict[(prev_end, '')] = {start, my_end_state}
-            context.dict[(end, '')] = {start, my_end_state}
+            context.add_transition(prev_end, '', {start, my_end_state})
+            context.add_transition(end, '', {start, my_end_state})
         return my_start_state, my_end_state
 
     @classmethod
     def _parse_ground(cls, node: GroundNode, context: Context) -> tuple[int, int]:
         my_start_state = context.get_new_state()
         my_end_state = context.get_new_state()
-        context.dict[(my_start_state, node.content)] = {my_end_state}
+        context.add_transition(my_start_state, node.content, {my_end_state})
         return my_start_state, my_end_state
 
     @classmethod
@@ -120,28 +128,78 @@ class ENFA:
 
 class NFA:
 
+    def __init__(self, start_state: int, end_states: set[int], states: range, dict_: dict[tuple[int, str], set[int]]):
+        assert states.start == 0
+        self.states = states
+        self.alphabet = {s for _, s in dict_.keys()}.difference({''})
+        self.start_state = start_state
+        self.end_states = end_states
+        self.dict = dict_
+
     @classmethod
     def from_enfa(cls, enfa: ENFA) -> 'NFA':
-        new_dict: dict[tuple[int, str], set[int]] = {}
+        e_closures = cls._compute_epsilon_closure(enfa)
 
-        adj_matrix = cls._compute_epsilon_closure(enfa)
+        new_dict: dict[tuple[int, str], set[int]] = {}
         for state in enfa.states:
             for letter in enfa.alphabet:
-                edom_state = [s for s in enfa.states if adj_matrix[s][state]]  # TODO: finish
+                transitioned = reduce(
+                    lambda s1, s2: s1.union(s2),
+                    (enfa.dict[(s, letter)] for s in e_closures[state] if (s, letter) in enfa.dict),
+                    set()
+                )
+                new_dict[(state, letter)] = set()
+                for s in transitioned:
+                    new_dict[(state, letter)].update(e_closures[s])
+                if len(new_dict[(state, letter)]) == 0:
+                    del new_dict[(state, letter)]
+
+        # TODO: delete unnecessary states
+
+        new_end_states: set[int] = set()
+        for state in enfa.states:
+            if enfa.end_state in e_closures[state]:
+                new_end_states.add(state)
+
+        return cls(enfa.start_state, new_end_states, enfa.states, new_dict)
 
     @classmethod
-    def _compute_epsilon_closure(cls, enfa: ENFA) -> list[list[bool]]:
+    def _compute_epsilon_closure(cls, enfa: ENFA) -> dict[int, set[int]]:
         adj_matrix = [[False] * len(enfa.states) for _ in enfa.states]
         for i in enfa.states:
             adj_matrix[i][i] = True
 
-        for (v, s), w in enfa.dict.items():
-            if s == '':
-                adj_matrix[v][w] = True
+        for (v, s), w_set in enfa.dict.items():
+            for w in w_set:
+                if s == '':
+                    adj_matrix[v][w] = True
 
         for k in enfa.states:
             for i in enfa.states:
                 for j in enfa.states:
                     adj_matrix[i][j] = adj_matrix[i][j] or (adj_matrix[i][k] and adj_matrix[k][j])
 
-        return adj_matrix
+        e_closures: dict[int, set[int]] = dict.fromkeys(enfa.states)
+        for state in enfa.states:
+            e_closures[state] = set(i for i in enfa.states if adj_matrix[state][i])
+
+        return e_closures
+
+
+class NFAIterator:
+
+    def __init__(self, nfa: 'NFA'):
+        self.nfa = nfa
+        self.states: frozenset[int] = frozenset({nfa.start_state})
+
+    def next(self, letter: str):
+        next_states = set()
+        for state in self.states:
+            next_states.update(self.nfa.dict.get((state, letter), set()))
+        self.states = frozenset(next_states)
+
+    def is_stuck(self) -> bool:
+        return len(self.states) != 0  # only works if unnecessary states are eliminated
+
+    def is_finished(self) -> bool:
+        return len(self.nfa.end_states.union(self.states)) != 0
---
+++ tests/test_nfa.py
@@ -0,0 +1,116 @@
+import unittest
+
+import htmplate.nfa as automata
+
+
+class ENFATest(unittest.TestCase):
+
+    def test_alt(self):
+        exp = automata.ReAlt(content=[
+            automata.GroundNode('a'),
+            automata.GroundNode('b'),
+            automata.GroundNode('c')
+        ])
+        enfa = automata.ENFA.parse(exp)
+        self.assertEqual({
+            (0, ''): {2, 4, 6},
+            (2, 'a'): {3},
+            (3, ''): {1},
+            (4, 'b'): {5},
+            (5, ''): {1},
+            (6, 'c'): {7},
+            (7, ''): {1},
+        }, enfa.dict)
+        self.assertEqual(0, enfa.start_state)
+        self.assertEqual(1, enfa.end_state)
+        self.assertEqual(set('abc'), enfa.alphabet)
+
+    def test_cat(self):
+        exp = automata.ReCat(content=[
+            automata.GroundNode('a'),
+            automata.GroundNode('b'),
+            automata.GroundNode('c')
+        ])
+        enfa = automata.ENFA.parse(exp)
+        self.assertEqual({
+            (0, ''): {2},
+            (2, 'a'): {3},
+            (3, ''): {4},
+            (4, 'b'): {5},
+            (5, ''): {6},
+            (6, 'c'): {7},
+            (7, ''): {1}
+        }, enfa.dict)
+
+    def test_item(self):
+        exp = automata.ReItem(content=automata.GroundNode('a'), min_=2, max_=4)
+        enfa = automata.ENFA.parse(exp)
+        self.assertEqual({
+            (0, ''): {2},
+            (2, 'a'): {3},
+            (3, ''): {4},
+            (4, 'a'): {5},
+            (5, ''): {1, 6},
+            (6, 'a'): {7},
+            (7, ''): {8, 1},
+            (8, 'a'): {9},
+            (9, ''): {1}
+        }, enfa.dict)
+
+        exp = automata.ReItem(content=automata.GroundNode('a'), min_=1, max_=1)
+        enfa = automata.ENFA.parse(exp)
+        self.assertEqual({
+            (0, ''): {2},
+            (2, 'a'): {3},
+            (3, ''): {1}
+        }, enfa.dict)
+
+        exp = automata.ReItem(content=automata.GroundNode('a'), min_=0, max_=float('inf'))
+        enfa = automata.ENFA.parse(exp)
+        self.assertEqual({
+            (0, ''): {1, 2},
+            (2, 'a'): {3},
+            (3, ''): {1, 2}
+        }, enfa.dict)
+
+    def test_recursion(self):
+        exp = automata.ReCat(content=[
+            automata.ReAlt(content=[
+                automata.GroundNode('a'),
+                automata.GroundNode('b')
+            ]),
+            automata.ReItem(content=automata.GroundNode('c'), min_=1, max_=2)
+        ])
+        enfa = automata.ENFA.parse(exp)
+        self.assertEqual({
+            (0, ''): {2},
+            (2, ''): {4, 6},
+            (3, ''): {8},
+            (4, 'a'): {5},
+            (5, ''): {3},
+            (6, 'b'): {7},
+            (7, ''): {3},
+            (8, ''): {10},
+            (9, ''): {1},
+            (10, 'c'): {11},
+            (11, ''): {9, 12},
+            (12, 'c'): {13},
+            (13, ''): {9}
+        }, enfa.dict)
+
+
+class NFATest(unittest.TestCase):
+
+    def test_alt(self):
+        exp = automata.ReAlt(content=[
+            automata.GroundNode('a'),
+            automata.GroundNode('b'),
+            automata.GroundNode('c')
+        ])
+        enfa = automata.ENFA.parse(exp)
+        nfa = automata.NFA.from_enfa(enfa)
+        self.assertEqual({}, nfa.dict)
+
+
+if __name__ == '__main__':
+    unittest.main()
